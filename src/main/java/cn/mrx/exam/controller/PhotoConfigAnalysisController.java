5a4aa426a344dc96686295c3e172d286f0b1eb9e
jxjy <mrx_email@163.com>
2017-05-18 20:14:57 +0800

    面部表情分析功能实现

--- src/main/java/cn/mrx/exam/controller/PhotoConfigAnalysisController.java
+++ src/main/java/cn/mrx/exam/controller/PhotoConfigAnalysisController.java
@@ -5,6 +5,7 @@ import cn.mrx.exam.pojo.PhotoConfig;
 import cn.mrx.exam.utils.BSGridPage;
 import cn.mrx.exam.utils.YoutuUtil;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -247,11 +249,18 @@ public class PhotoConfigAnalysisController extends BaseController{
      */
     @RequestMapping(value = "/processFace/{id}", method = RequestMethod.GET)
     public String analysisProcess(@PathVariable("id") String id, Model model){
-        model.addAttribute("id", id);
+        List<Integer> expression = new ArrayList<>();
         List<Photo> photos = selectPhotos(id);
         for (Photo photo : photos){
-            System.out.println(photo.getResultDetectface());
+            JSONObject jsonObject = JSON.parseObject(photo.getResultDetectface());
+            if(jsonObject.get("face")!=null){
+                JSONArray jsonArray = JSON.parseArray(jsonObject.get("face").toString());
+                JSONObject jsonObject2 = (JSONObject)jsonArray.get(0);
+                expression.add((int)jsonObject2.get("expression"));
+            }
         }
+        model.addAttribute("expression", expression);
+        model.addAttribute("id", id);
         return "admin/photoConfigAnalysis/processFace";
     }
 }
--- src/main/webapp/WEB-INF/jsp/admin/photoConfigAnalysis/processFace.jsp
+++ src/main/webapp/WEB-INF/jsp/admin/photoConfigAnalysis/processFace.jsp
@@ -19,13 +19,53 @@
 	<!-- 主体 -->
 	<div class="page-container">
 		<%@ include file="process-menu.jspf" %>
+		<div id="container" style="min-width:700px;height:400px"></div>
 	</div>
 
 	<!-- js -->
 	<%@ include file="../jspf/footer.jspf" %>
+	<script type="text/javascript" src="${ctx}/resources/admin/h-ui/lib/hcharts/Highcharts/5.0.6/js/highcharts.js"></script>
+	<script type="text/javascript" src="${ctx}/resources/admin/h-ui/lib/hcharts/Highcharts/5.0.6/js/modules/exporting.js"></script>
 
 	<!-- 自定义js -->
 	<script type="text/javascript">
+		$(function () {
+			var expression = ${expression};
+
+			Highcharts.chart('container', {
+				title: {
+					text: '面部表情走势图',
+					x: -20 //center
+				},
+				subtitle: {
+					text: 'Source: www.xlbweb.com',
+					x: -20
+				},
+				yAxis: {
+					title: {
+						text: 'Expression'
+					},
+					plotLines: [{
+						value: 0,
+						width: 1,
+						color: '#808080'
+					}]
+				},
+				tooltip: {
+					valueSuffix: '°'
+				},
+				legend: {
+					layout: 'vertical',
+					align: 'right',
+					verticalAlign: 'middle',
+					borderWidth: 0
+				},
+				series: [{
+					name: 'admin',
+					data: expression
+				}]
+			});
+		});
 	</script>
 </body>
 </html>
