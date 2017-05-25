b75359d152be6e912dc463ff5b3e6ebce02d7082
jxjy <mrx_email@163.com>
2017-05-25 17:46:57 +0800

    系统设置-网站设置模块功能完成

--- src/main/java/cn/mrx/exam/controller/SystemWebController.java
+++ src/main/java/cn/mrx/exam/controller/SystemWebController.java
@@ -1,8 +1,16 @@
 package cn.mrx.exam.controller;
 
+import cn.mrx.exam.pojo.SystemWeb;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import cn.mrx.exam.controller.BaseController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,7 +21,59 @@ import cn.mrx.exam.controller.BaseController;
  * @since 2017-03-26
  */
 @Controller
-@RequestMapping("/systemWeb")
+@RequestMapping("/admin/systemWeb")
 public class SystemWebController extends BaseController {
-	
+
+    /**
+     * 打开systemWeb-list页面
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "", method = RequestMethod.GET)
+	public String systemWeb(Model model){
+        List<SystemWeb> systemWebList = iSystemWebService.selectList(null);
+        model.addAttribute("systemWebList", systemWebList);
+        return "admin/systemWeb/systemWeb-list";
+    }
+
+    /**
+     * 打开systemWeb-edit页面
+     * @param id
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable("id") String id,
+                       Model model){
+        EntityWrapper<SystemWeb> entityWrapper = new EntityWrapper<>();
+        entityWrapper.eq("category", id);
+        SystemWeb systemWeb = iSystemWebService.selectOne(entityWrapper);
+        model.addAttribute("systemWeb", systemWeb);
+        return "admin/systemWeb/systemWeb-edit";
+    }
+
+    /**
+     * 异步修改网站设置
+     * @param id
+     * @param keywords
+     * @param description
+     * @param title
+     * @param version
+     * @return
+     */
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @ResponseBody
+    public Object edit(String id,
+                       String keywords,
+                       String description,
+                       String title,
+                       String version){
+        SystemWeb systemWeb = new SystemWeb();
+        systemWeb.setId(Integer.valueOf(id));
+        systemWeb.setKeywords(keywords);
+        systemWeb.setDescription(description);
+        systemWeb.setTitle(title);
+        systemWeb.setVersion(version);
+        return iSystemWebService.updateById(systemWeb);
+    }
 }
---
+++ src/main/webapp/WEB-INF/jsp/admin/systemWeb/systemWeb-list.jsp
@@ -0,0 +1,59 @@
+﻿<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ include file="../jspf/taglib.jspf"%>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<%@ include file="../jspf/head.jspf" %>
+</head>
+<body>
+<nav class="breadcrumb"><i class="Hui-iconfont">&#xe67f;</i> 首页 <span class="c-gray en">&gt;</span> 系统设置 <span class="c-gray en">&gt;</span> 网站设置 <a class="btn btn-success radius r" style="line-height:1.6em;margin-top:3px" href="javascript:location.replace(location.href);" title="刷新" ><i class="Hui-iconfont">&#xe68f;</i></a></nav>
+<div class="page-container">
+	<table class="table table-border table-bordered table-hover table-bg">
+		<thead>
+			<tr>
+				<th scope="col" colspan="7">网站设置</th>
+			</tr>
+			<tr class="text-c">
+				<th>ID</th>
+				<th>关键词</th>
+				<th>描述</th>
+				<th>TAB标题</th>
+				<th>版本</th>
+				<th>类别</th>
+				<th>操作</th>
+			</tr>
+		</thead>
+		<tbody>
+			<c:forEach items="${systemWebList}" var="c">
+				<tr class="text-c">
+					<td>${c.id}</td>
+					<td>${c.keywords}</td>
+					<td>${c.description}</td>
+					<td>${c.title}</td>
+					<td>${c.version}</td>
+					<td>
+						<c:if test="${c.category==1}">前台网页设置</c:if>
+						<c:if test="${c.category==2}">后台网页设置</c:if>
+					</td>
+					<td class="f-14">
+						<a title="编辑" href="javascript:;" onclick="systemWeb_edit('网站设置编辑','${ctx}/admin/systemWeb/edit/${c.id}')" style="text-decoration:none"><i class="Hui-iconfont">&#xe6df;</i></a>
+					</td>
+				</tr>
+			</c:forEach>
+		</tbody>
+	</table>
+</div>
+	<%@ include file="../jspf/footer.jspf" %>
+	<script type="text/javascript" src="${ctx}/resources/admin/h-ui/lib/datatables/1.10.0/jquery.dataTables.min.js"></script>
+	<script type="text/javascript">
+		/**
+		 * 网站设置-编辑
+		 * @param title
+         * @param url
+         */
+		function systemWeb_edit(title,url){
+			layer_show(title,url);
+		}
+	</script>
+</body>
+</html>
